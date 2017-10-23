2409e4bb22913f9d6cf32bb23a3bce5b168b3f04
ukimiawz <deathmoon123>
2017-10-24 00:59:19 +0200

    Generated microservice version 1.0

---
+++ db/Test_Image_2_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  imageDatabase
+-- Automatically generated sql script for the service Test Image 2, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table imageTable.
+--
+CREATE TABLE imageDatabase.imageTable (
+  imageName VARCHAR(255) ,
+  imageId VARCHAR(255) ,
+CONSTRAINT imageId_PK PRIMARY KEY (imageId)
+ 
+);
+
+
+
